35b536c407dd91f0f84d3ff9298cf2bf486a9535
casjay <git-admin@casjaysdev.pro>
2025-02-03 15:19:34 -0500

    🗃️ Committing everything that changed 🗃️
    Dockerfile
    .env.scripts
    rootfs/usr/local/etc/docker/init.d/00-enclosed.sh
    rootfs/usr/local/share/template-files/config/env/default.sample

--- rootfs/usr/local/etc/docker/init.d/00-enclosed.sh
+++ rootfs/usr/local/etc/docker/init.d/00-enclosed.sh
@@ -76,7 +76,7 @@ DATABASE_SUBDIR="enclosed"
 DATABASE_DIR="$DATABASE_BASE_DIR/$DATABASE_SUBDIR"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Set webroot
-WWW_ROOT_DIR="/usr/share/httpd/default"
+WWW_ROOT_DIR="/usr/local/share/httpd/default"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Default predefined variables
 DATA_DIR="/data/enclosed"   # set data directory
